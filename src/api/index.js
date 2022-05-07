5e871ff71aef084b612e9662ba8ae3161323029b
Jose Suresh Alejo Medina <suale96@gmail.com>
2022-05-07 14:13:45 +0100

    Added CRUD requests, modified save/read file functions and added delete file

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,78 @@
-import express from 'express'
-import {helloWorld} from '@/api/controllers'
+import express from "express"
+import bodyParser from "body-parser"
+import { saveStorageLocalFile } from "@/api/controllers/storageController"
+import { saveUserLocalFile } from "@/api/controllers/userController"
+import { readLocalFile, deleteLocalFile } from "./controllers/main"
 
 const app = express()
 const port = 8090
 
-app.get('/', helloWorld)
+app.use(bodyParser.json({ type: "application/json" }))
+
+app.post("/account", function (req, res) {
+  const storage = req.body.storage
+  const fileName = req.body.fileName
+  try {
+    saveStorageLocalFile(storage, fileName)
+    res.sendStatus(200)
+  } catch (e) {
+    res.status(400).send(e.message)
+  }
+})
+
+app.post("/user", function (req, res) {
+  const user = req.body.user
+  const fileName = req.body.fileName
+  try {
+    saveUserLocalFile(user, fileName)
+    res.sendStatus(200)
+  } catch (e) {
+    res.status(400).send(e.message)
+  }
+})
+
+app.get("/user/:fileName", function (req, res) {
+  const fileName = req.params.fileName
+  try {
+    const file = readLocalFile(fileName)
+    res.status(200).json(file)
+  } catch (e) {
+    res.status(400).send(e.message)
+  }
+})
+
+app.get("/account/:fileName", function (req, res) {
+  const fileName = req.params.fileName
+  try {
+    const file = readLocalFile(fileName)
+    res.status(200).json(file)
+  } catch (e) {
+    res.status(400).send(e.message)
+  }
+})
+
+app.delete("/user/:fileName", function(req, res) {
+  const fileName = req.params.fileName
+  try {
+    deleteLocalFile(fileName)
+    res.sendStatus(200)
+  } catch (e) {
+    res.status(400).send(e.message)
+  }
+})
+
+app.delete("/account/:fileName", function(req, res) {
+  const fileName = req.params.fileName
+  try {
+    deleteLocalFile(fileName)
+    res.sendStatus(200)
+  } catch (e) {
+    res.status(400).send(e.message)
+  }
+})
 
 export default () => {
-    app.listen(port, () => {
-        console.log(`Example app listening on port ${port}`)
-    })
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
 }
