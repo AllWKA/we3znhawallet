f6600f10753d60298974808e43a9c0a74fc73e6d
AllWKA <bryanjaramillobaldeon@gmail.com>
2022-05-08 13:39:00 +0100

    create general modal component #28

---
+++ src/components/Modal.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="modal-container" v-if="show">
+    <div class="modal-content-container">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+  props: {
+    show: Boolean
+  }
+}
+</script>
+
+<style scoped>
+
+.modal-container {
+  position: fixed;
+  color: red;
+  left: 0;
+  top: 0;
+  height: 100%;
+  width: 100%;
+}
+
+.modal-content-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 99;
+  height: 100%;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.64);
+}
+</style>
