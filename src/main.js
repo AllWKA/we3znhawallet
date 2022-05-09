215a1b7e33ecf460b84ef229de2cb65eeaaf945f
AllWKA <bryanjaramillobaldeon@gmail.com>
2022-05-09 18:39:28 +0100

    fix Axios reference #18

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:8090'
+})
 
 Vue.config.productionTip = false
+Vue.prototype.$axios = axiosInstance
 
 new Vue({
   router,
