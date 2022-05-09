95698fe84955b4b4b37378250b74adde4e279a6d
Jose Suresh Alejo Medina <suale96@gmail.com>
2022-05-09 19:33:20 +0100

    Added menu nav bar and menu nav bar link components, added new root styles and modified styles in views

---
+++ src/components/menu-nav-bar/MenuNavBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="sidebar">
+    <MenuNavBarLink to="/" name="Login" />
+    <MenuNavBarLink to="/home" name="Home" />
+  </div>
+</template>
+
+<script>
+import MenuNavBarLink from "@/components/menu-nav-bar/MenuNavBarLink.vue"
+
+export default {
+  name: "MenuNavBar",
+  components: {
+    MenuNavBarLink,
+  },
+}
+</script>
+
+<style>
+.sidebar {
+  background-color: var(--menu-nav-bar-bg-color);
+  width: 25%;
+  height: 100%;
+  overflow: hidden;
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,24 @@
 <template>
-  <AccountBankList />
+  <div class="home">
+    <AccountBankList />
+  </div>
 </template>
 
 <script>
-import AccountBankList from '@/components/AccountBankList.vue'
+import AccountBankList from "@/components/AccountBankList.vue"
 
 export default {
   name: "Home",
   components: {
-    AccountBankList,
-  },
+    AccountBankList
+  }
 }
 </script>
+
+<style>
+.home {
+  width: 100%;
+  height: 100%;
+  display: flex;
+}
+</style>
