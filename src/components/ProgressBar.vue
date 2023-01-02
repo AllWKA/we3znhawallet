2546a38b3e7ee3f27fa8712169bb73886e18c929
AllWKA <bryanjaramillobaldeon@gmail.com>
2023-01-02 04:03:25 +0000

    create progress bar for budgets and add examples

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div style="display: flex; width: 100%">
+    <div :style="`text-align:center;background-color: ${progressColor};width:${progressPercentage}%`">
+      {{ progressPercentage }}% - {{ progress }}€
+    </div>
+
+    <div :style="`text-align:center;background-color: ${totalColor};width:${totalPercentageLeft}%; min-width: 30%`">
+      {{ totalPercentageLeft }}% - {{ total }}€
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProgressBar",
+  props: {
+    progressColor: String,
+    totalColor: String,
+    progress: Number,
+    total: Number
+  },
+  data() {
+    return {
+      progressPercentage: 0,
+      totalPercentageLeft: 0
+    }
+  },
+  beforeMount() {
+    this.progressPercentage = this.progress * 100 / this.total
+
+    this.totalPercentageLeft = 100 - this.progressPercentage
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/views/Account.vue
+++ src/views/Account.vue
@@ -1,16 +1,43 @@
 <template>
-  <div>
-    <h1>{{ account.cardNumbers }}</h1>
-    <h3>{{ account.currentBalance }} €</h3>
+  <div style="display: flex; flex-direction: column; height: 100%; width: 100%">
+    <div style="min-height: 15%">
+      <h1 style="text-align: center; width: 100%">Account: {{ account.cardNumbers }}</h1>
+      <h2 style="text-align: center; width: 100%">Saldo: {{ account.currentBalance }} €</h2>
+    </div>
+
+    <div style="display: flex; justify-content: space-around; width: 100%">
+      <div style="display: flex; flex-direction: column; width: 30%">
+        <h3>Presupuesto 1</h3>
+        <ProgressBar :progress="30" :total="100" progress-color="red" total-color="yellow"/>
+      </div>
+
+      <div style="display: flex; flex-direction: column; width: 30%">
+        <h3>Presupuesto 2</h3>
+        <ProgressBar :progress="45" :total="100" progress-color="red" total-color="yellow"/>
+      </div>
+
+      <div style="display: flex; flex-direction: column; width: 30%">
+        <h3>Presupuesto 3</h3>
+        <ProgressBar :progress="97" :total="100" progress-color="red" total-color="yellow"/>
+      </div>
+    </div>
+
+    <div style="min-height: 60%"></div>
   </div>
 </template>
 
 <script>
+import ProgressBar from "@/components/ProgressBar";
+
 export default {
   name: "Account",
+  components: {
+    ProgressBar
+  },
   data() {
     return {
-      account: Object
+      account: Object,
+      chart: Object
     }
   },
   async beforeMount() {
