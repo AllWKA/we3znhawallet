408dd217d3580352371d7fd91b7ddc3862eff46a
AllWKA <bryanjaramillobaldeon@gmail.com>
2023-06-07 12:38:35 +0100

    reorder enpoints

--- src/api/index.js
+++ src/api/index.js
@@ -2,12 +2,18 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import * as controllers from './controllers/main'
-import {addMovementsInAccount, createNewBudget, deleteBudget, processBankAccountMovements, updateBudget} from './controllers/main'
+import {
+  addMovementsInAccount,
+  createNewBudget,
+  deleteBudget,
+  processBankAccountMovements,
+  updateBudget
+} from './controllers/main'
 
 const app = express()
 const port = process.env.SERVER_PORT
 
-app.use(bodyParser.json({type: 'application/json', limit: '100mb'}))
+app.use(bodyParser.json({ type: 'application/json', limit: '100mb' }))
 
 app.use(cors())
 
@@ -28,6 +34,71 @@ app.get('/account/:id', (req, res) => {
     res.json(account)
   } catch (e) {
     res.status(400).send(e.message)
+  }
+})
+
+app.get('/user/:fileName', (req, res) => {
+  const fileName = req.params.fileName
+  try {
+    const file = controllers.readLocalFile(fileName)
+    res.status(200).json(file)
+  } catch (e) {
+    res.status(400).send(e.message)
+  }
+})
+
+app.get('/isSignedIn', controllers.isSignedIn)
+
+app.get('/validate/token', (req, res) => res.send())
+
+// PUTS
+
+app.put('/account/budget/:accountId', (req, res) => {
+  try {
+    updateBudget(req.body, req.params.accountId)
+
+    res.send(204)
+  } catch (err) {
+    res.send(err)
+  }
+})
+
+// POSTS
+
+app.post('/user', (req, res) => {
+  const user = req.body.user
+  const fileName = req.body.fileName
+  try {
+    controllers.saveUserLocalFile(user, fileName)
+    res.sendStatus(200)
+  } catch (e) {
+    res.status(400).send(e.message)
+  }
+})
+
+app.post('/signin', (req, res) => {
+  const pin = req.body.pin
+
+  const verificationPin = req.body.verificationPin
+
+  if (pin !== verificationPin) {
+    res.status(400).send('Pin and verification Pin are different')
+  }
+
+  try {
+    controllers.signin(pin, res)
+  } catch (e) {
+    res.status(400).send(`Can not create pin: ${e.message}`)
+  }
+})
+
+app.post('/login', (req, res) => {
+  const pin = req.body.pin
+
+  try {
+    controllers.login(pin, res)
+  } catch (e) {
+    res.status(400).send(`Unable to login: ${e.message}`)
   }
 })
 
@@ -87,15 +158,7 @@ app.post('/account/budget/delete/:accountId', (req, res) => {
   }
 })
 
-app.put('/account/budget/:accountId', (req, res) => {
-  try {
-    updateBudget(req.body, req.params.accountId)
-
-    res.send(204)
-  } catch (err) {
-    res.send(err)
-  }
-})
+// DELETES
 
 app.delete('/account/:id', (req, res) => {
   const accountId = req.params.id
@@ -104,27 +167,6 @@ app.delete('/account/:id', (req, res) => {
     controllers.deleteAccount(accountId)
 
     res.sendStatus(200)
-  } catch (e) {
-    res.status(400).send(e.message)
-  }
-})
-
-app.post('/user', (req, res) => {
-  const user = req.body.user
-  const fileName = req.body.fileName
-  try {
-    controllers.saveUserLocalFile(user, fileName)
-    res.sendStatus(200)
-  } catch (e) {
-    res.status(400).send(e.message)
-  }
-})
-
-app.get('/user/:fileName', (req, res) => {
-  const fileName = req.params.fileName
-  try {
-    const file = controllers.readLocalFile(fileName)
-    res.status(200).json(file)
   } catch (e) {
     res.status(400).send(e.message)
   }
@@ -140,35 +182,6 @@ app.delete('/user/:fileName', (req, res) => {
   }
 })
 
-app.post('/signin', (req, res) => {
-  const pin = req.body.pin
-
-  const verificationPin = req.body.verificationPin
-
-  if (pin !== verificationPin) {
-    res.status(400).send('Pin and verification Pin are different')
-  }
-
-  try {
-    controllers.signin(pin, res)
-  } catch (e) {
-    res.status(400).send(`Can not create pin: ${e.message}`)
-  }
-})
-
-app.get('/isSignedIn', controllers.isSignedIn)
-
-app.post('/login', (req, res) => {
-  const pin = req.body.pin
-
-  try {
-    controllers.login(pin, res)
-  } catch (e) {
-    res.status(400).send(`Unable to login: ${e.message}`)
-  }
-})
-
-app.get('/validate/token', (req, res) => res.send())
 
 export default () => {
   app.listen(port, () => {
